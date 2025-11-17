82098579bcf1b4d28fa89740e630e45fe026edf9
Shajidaa <shajidaakterlopa101@gmail.com>
2025-11-17 12:30:24 +0600

    firebase init

--- src/Pages/Auth/Login/Login.jsx
+++ src/Pages/Auth/Login/Login.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
   return (
     <div>
       {" "}
-      <fieldset className="fieldset">
-        <label className="label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
-        <label className="label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
-        <div>
-          <a className="link link-hover">Forgot password?</a>
-        </div>
-        <button className="btn btn-neutral mt-4">Login</button>
-      </fieldset>
+      <form>
+        <h1 className="text-center font-bold text-3xl">Welcome Back </h1>
+        <p className="text-center text-gray-400 font-semibold text-lg">
+          Login with ZapShift
+        </p>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+
+          <Link to={"/register"} className="link link-hover">
+            Register
+          </Link>
+        </fieldset>
+      </form>
     </div>
   );
 };
--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-  return <div>Register</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleRegistration = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handleRegistration)}>
+        <h1 className="text-center font-bold text-3xl">Welcome Back </h1>
+        <p className="text-center text-gray-400 font-semibold text-lg">
+          Register with ZapShift
+        </p>
+        <fieldset className="fieldset">
+          {/* email */}
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500"> Email is required</p>
+          )}
+          {/* password */}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: 8,
+              pattern:
+                /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/,
+            })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p role="alter" className="text-red-500">
+              {" "}
+              Password is required
+            </p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">
+              {" "}
+              Password must be 8 characters or longer
+            </p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-500">
+              {" "}
+              Password must have at least one uppercase ,at least one lowercase
+              ,at least one number , and at least one special characters.
+            </p>
+          )}
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
