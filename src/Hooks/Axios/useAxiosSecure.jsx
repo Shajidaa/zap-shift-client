d523cb5e9d89776996bf4289c8fe429e3ea194a2
Shajidaa <shajidaakterlopa101@gmail.com>
2025-11-23 12:28:40 +0600

    jwt

--- src/Hooks/Axios/useAxiosSecure.jsx
+++ src/Hooks/Axios/useAxiosSecure.jsx
@@ -1,9 +1,40 @@
 import axios from "axios";
+import { use, useEffect } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 const useAxiosSecure = () => {
+  const { user, logOut } = use(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const axiosInterceptor = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (err) => {
+        console.log(err);
+        const statusCode = err.status;
+        if (statusCode === 401 || statusCode === 403) {
+          logOut().then(() => {
+            navigate("/login");
+          });
+        }
+        return Promise.reject(err);
+      }
+    );
+    return () => {
+      axios.interceptors.request.eject(axiosInterceptor);
+      axios.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, navigate, logOut]);
   return axiosSecure;
 };
 
